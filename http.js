551dc196aaf77752d8e9b7f93f0eadab6add698c
Senthee1811 <esentheepan@gmail.com>
2024-07-10 10:23:45 +0530

    Concepts

--- http.js
+++ http.js
@@ -1,7 +1,18 @@
 const http = require('http'); 
 
-http.createServer(function(req,res){
-    res.write("Hello World"); //Writes a respond to the Client
-    res.end();//Ends the response 
-}).listen(8080) //Server listens in the port 8080 
+ const sever = http.createServer(function(req,res){
+    if (req.url === '/'){
+        res.end("This is Home Page")
+    }
+    if(req.url === '/about'){
+        res.end("This is about page")
+    }
+    res.end(`
+        <h1>Oops</h1>
+        <p>We didnt found that page!!!!</p>
+        
+        `)
+})
+
+sever.listen(8080) //Server listens in the port 8080 
 
