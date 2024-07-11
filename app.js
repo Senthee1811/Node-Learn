41f8cf5a6591951ae8798f5314d4354d91fe383d
Senthee1811 <esentheepan@gmail.com>
2024-07-11 09:39:24 +0530

    Fixed Path & added Event

---
+++ app.js
@@ -0,0 +1,14 @@
+const {readFile} = require('fs'); 
+
+console.log('First Task'); 
+
+readFile('./content/first.txt','utf8',(err,result) => {
+    if(err){
+        console.log(err); 
+        return
+    }
+    console.log(result); 
+    console.log('Completed First Task'); 
+
+})
+console.log("Starting next Task");
--- path.js
+++ path.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 console.log(path.sep);
 
-const filePath = path.join('/content\subFolder','sample.txt')
+const filePath = path.join('./content','first.txt')
 console.log(filePath)
 
 const base = path.basename(filePath)
