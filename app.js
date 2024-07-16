5c23207e5a23dc466c030286f331fb4fb333ef55
Senthee1811 <esentheepan@gmail.com>
2024-07-16 10:35:13 +0530

    Added Nodemon and Modified

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+const {readFile} = require('fs'); 
+
+const getText = (path) => {
+    return new Promise ((resolve,reject) => {
+readFile('./content/first.txt','utf8',(err,data) => {
+     if(err){
+        reject(err)
+     }
+     else{
+        resolve(data)
+     }
+}) 
+
+    })
+}
+
+// getText('./content/first.txt').then((result) => console.log(result))
+// .catch((err) => console.log(err) )
+
+
+const start = async() => {
+    try{
+        const first = await getText('./content/first.txt');
+        const second = await getText('./content/second.txt'); 
+        console.log(first,second);
+        
+
+    } catch(error){
+        console.log(error);
+
+    }
+}
+
+start()
 
