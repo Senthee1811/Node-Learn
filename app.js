9d0d9279b624b3ae63e078b41ad520562f6a8cd5
Senthee1811 <esentheepan@gmail.com>
2024-07-16 21:54:37 +0530

    Event emitter competed

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const EventEmitter = require('events')
+const http = require('http')
+// const EventEmitter = require('events')
 
-const customEmitter = new EventEmitter(); 
+// const customEmitter = new EventEmitter(); 
 
-customEmitter.on('response',(name,id) => {
-    console.log(`Data received user ${name} with ${id}`);
+// customEmitter.on('response',(name,id) => {
+//     console.log(`Data received user ${name} with ${id}`);
+// })
+// customEmitter.on('response',() => {
+//     console.log(`Some other Logic`);
+// })
+// customEmitter.emit('response',"John",34);
+
+const server = http.createServer()
+
+server.on('request',(req,res) => {
+    res.end("Welcome")
 })
-customEmitter.on('response',() => {
-    console.log(`Some other Logic`);
-})
-customEmitter.emit('response',"John",34);
+
+server.listen(5000);
--- event.js
+++ event.js
@@ -9,3 +9,12 @@ customEmitter.on('response',() => {
     console.log(`Some other Logic`);
 })
 customEmitter.emit('response',"John",34);
+
+//Sever
+// const server = http.createServer()
+
+// server.on('request',(req,res) => {
+//     res.end("Welcome")
+// })
+
+// server.listen(5000);
