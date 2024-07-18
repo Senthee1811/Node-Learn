cd46d38195d35163a63ce3c28c2ef73928ba40fb
Senthee1811 <esentheepan@gmail.com>
2024-07-18 15:42:45 +0530

    Express Middleware

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-const express = require('express') 
-const app = express() 
- 
-app.get('/',(req,res)=>{
-    res.send("HomePage")
-}) 
+const express = require('express'); 
+const app = express(); 
+const logger = require('./logger.js')
+//req => middleware => res 
 
-app.get('/about',(req,res) => {
-    res.send("About Page")
+
+
+app.get('/',logger,(req,res)=>{
+    res.send("Home")
 })
 
-app.all('*',(req,res)=>{
-    res.status(404).send('<h1>Resource Not FOUND!!!</h1>')
+app.get('/about',(req,res)=>{
+    res.send("About")
 })
 
 
 
-app.listen(5000,()=> {
-    console.log("Server is listening on port number 5000")
-})
 
-//app.get()
-//app.post()
-//app.put()
-//app.delete()
-//app.all()
-//app.use() -> For Middleware
+
+
+
+
+app.listen(5000,()=>{
+    console.log('Server is running')
+})
